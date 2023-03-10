13371886b7aada038166d95057f1c498f0ac37c6
bsw98 <qwqq6954@naver.com>
2023-03-10 18:10:52 +0900

    [setting] babel macro delete

--- potato-market/vite.config.js
+++ potato-market/vite.config.js
@@ -1,15 +1,14 @@
 import * as path from 'node:path';
 
-import { defineConfig } from 'vite';
-import { resolve } from 'node:path';
 import react from '@vitejs/plugin-react-swc';
-import macro from 'vite-plugin-babel-macros';
+import { defineConfig } from 'vite';
+
+
 
 
 
 export default defineConfig({
   plugins: [
-    macro(), 
     react()
   ],
   resolve: {
