2c0ac0f166225199f2e0ead189c00918d7ee3363
Dongryul KIM <doctor.k1994@gmail.com>
2023-03-15 14:43:32 +0900

    [markup&styles] githublogo 추가 & 푸터 상단 기본 마크업

--- potato-market/src/components/Footer.jsx
+++ potato-market/src/components/Footer.jsx
@@ -1,6 +1,21 @@
+import githubLogo from "../assets/github-logo-vector.svg" ; 
+
 const Footer = () => {
   return (
-    <h2>Footer</h2>
+    <>
+    <ul>
+      <li>중고거래</li>
+      <li>매물 등록하기</li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+
+    <figure>
+      <figcaption>감자마켓 다운로드</figcaption>
+      <img alt="" src={githubLogo}/>
+    </figure>
+  </>
   )
 };
 
