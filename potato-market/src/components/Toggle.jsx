363b77a623df0dd4338beff7f595f0aa78e603b6
doctork4 <doctor.k1994@gmail.com>
2023-03-23 07:39:20 +0900

    [markup&styles] 로그아웃 시 토글박스 마크업스타일링  완료 #5

--- potato-market/src/components/Toggle.jsx
+++ potato-market/src/components/Toggle.jsx
@@ -1,18 +1,49 @@
+import { Link } from 'react-router-dom';
+
 import styled from "styled-components";
+
+import toggleBox from "@/assets/header_toggle.svg";
 
 function Toggle(){
   return (
 		<Div>
 			<ul>
-				<li>로그인</li>
-				<li>회원가입</li>
+				{/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+				<li><Link className="textLink" to={"/SignIn"}>로그인</Link></li>
+				{/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+				<li><Link className="textLink" to={"/SignUp"}>회원가입</Link></li>
 			</ul>
 		</Div>
 	)
 };
 
 const Div = styled.div`
-  background-image: url("../assets/header_toggle.svg");
+  background-image: url(${toggleBox});
+	background-position: center;
+	background-repeat: no-repeat;
+	background-size: cover;
+	width: 213px;
+	height: 135px;
+	position: absolute;
+	right: 555px;
+	top: 53px;
+	padding : 32px 40px;
+	box-sizing: border-box;
+	
+	ul {
+		font-size: 1.125rem;
+		font-weight: 700;
+		line-height: 2.2rem;
+	}
+
+	.textLink {
+			text-decoration: none;
+			color: #212325;
+
+			&:focus, &:hover, &:visited, &:link, &:active {
+				text-decoration: none;
+			}
+	}
 `;
 
 export default Toggle;
