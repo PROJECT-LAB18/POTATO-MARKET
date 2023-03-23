0c495d769be5549cdbe7be84e8305b9816f867ba
bsw98 <qwqq6954@naver.com>
2023-03-23 16:06:43 +0900

    [feature,style] 로그아웃상태로 글쓰기 접근 불가 구현

--- potato-market/src/pages/WriteArticle.jsx
+++ potato-market/src/pages/WriteArticle.jsx
@@ -1,17 +1,33 @@
 import { useRecoilState } from "recoil";
 import styled from 'styled-components';
 
+import { useNavigate } from 'react-router-dom';
+
 import WriteForm from '@/components/WriteForm';
 import { userId } from "@/stores/userAuth.js"
 import {ContainerGlobalStyle} from '@/styles/ContainerGlobalStyle';
+import { CustomButton } from "./productdetail";
 function WriteArticle(){
-  const [login] = useRecoilState(userId);
-  
+  const [login,setLogin] = useRecoilState(userId);
+  const navigate = useNavigate();
   return (
     <Main className="wrapper">
       <ContainerGlobalStyle />
       {login==null?
-      (<h1>로그인하고 사용해라</h1>):
+      (
+      <>
+        <h2 className="error-massage">로그인 후 사용해주세요.</h2>
+        <div className="button-div">
+         <CustomButton onClick={
+          ()=>{navigate('/signin')}
+         } red>확인</CustomButton>
+         <CustomButton onClick={
+          ()=>{navigate(-1)}
+         }>취소</CustomButton>
+        </div>
+      </>
+      ):
+      
       (
         <>
           <h2 className="articleTitle">게시글 작성</h2>
@@ -24,9 +40,22 @@ function WriteArticle(){
 }
 
 const Main = styled.main`
+  display:flex;
+  flex-flow:column;
+  justify-content: center;
+  align-items: center;
   width: 886px;
   margin-left: auto;
   margin-right: auto;
+  & .error-massage{
+    font-size:32px;
+    font-weight: 700;
+    margin-bottom:24px;
+  }
+  & .button-div{
+    display:flex;
+    gap:12px;
+  }
 `
 
 export default WriteArticle;
