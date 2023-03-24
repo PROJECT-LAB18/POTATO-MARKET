cf2d0845a1ef5f6a9c6bd4b1ba9a8ffe8b04be87
sryung <sryung1225@gmail.com>
2023-03-24 12:41:27 +0900

    [enhancement] 로그아웃 시, userInfo에 담긴 내용도 비워줌

--- potato-market/src/components/LogoutButton.jsx
+++ potato-market/src/components/LogoutButton.jsx
@@ -1,16 +1,20 @@
+import { useRecoilState } from "recoil";
+
 import firebase from '@/firebase';
+import { userInformation } from "@/stores/userAuth.js"
 
 const auth = firebase.auth();
 
-const handleLogout = async () => {
-  try {
-    await auth.signOut();
-  } catch (error) {
-    console.error(error);
-  }
-}
-
 function LogoutButton() {
+  const [userInfo, setUserInfo] = useRecoilState(userInformation);
+  const handleLogout = async () => {
+    try {
+      await auth.signOut();
+      setUserInfo(null);
+    } catch (error) {
+      console.error(error);
+    }
+  }
   return (
     <button type="button" onClick={handleLogout}>로그아웃</button>
   )
