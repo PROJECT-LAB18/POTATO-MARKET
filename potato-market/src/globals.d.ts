e0fe001c21a8d69c6c8cbc3ec269756f6ded2ee0
sryung <sryung1225@gmail.com>
2023-04-13 17:21:42 +0900

    [migration] globals.d.ts - svg, png 경로 대응을 위한 globals.d.ts 파일 추가

---
+++ potato-market/src/globals.d.ts
@@ -0,0 +1,9 @@
+// @types/global/index.d.ts
+declare module "*.png";
+declare module "*.svg" {
+  import React = require("react");
+
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
