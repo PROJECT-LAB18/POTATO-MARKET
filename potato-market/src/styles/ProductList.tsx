4c44dc58c67eeff4542b41e57cb153c584788f85
sryung <sryung1225@gmail.com>
2023-04-14 16:16:50 +0900

    [migration] styles 폴더 스타일드 컴포넌트 파일들 변환 - @typescript-eslint/parser 설치 - js->tsx 파일 변환 - FormButton, CustomButton 내 props 에러 대처를 위한 interface 추가

--- potato-market/src/styles/ProductList.tsx
+++ potato-market/src/styles/ProductList.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const ProductList = styled.section`
   margin: 35px auto;
@@ -6,19 +6,19 @@ const ProductList = styled.section`
   gap: 55px;
   grid-template-columns: repeat(4, 1fr);
   grid-template-rows: 309px;
-  & .loading{
-    left:43%;
+  & .loading {
+    left: 43%;
     position: absolute;
   }
-  @media (max-width: 480px){
+  @media (max-width: 480px) {
     grid-template-columns: repeat(1, 1fr);
   }
-  @media (min-width: 481px) and (max-width: 767px){
+  @media (min-width: 481px) and (max-width: 767px) {
     grid-template-columns: repeat(2, 1fr);
   }
-  @media (min-width: 768px) and (max-width: 1023px){
+  @media (min-width: 768px) and (max-width: 1023px) {
     grid-template-columns: repeat(3, 1fr);
   }
-`
+`;
 
 export default ProductList;
