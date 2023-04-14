2fc24ab97262c4d577c15d4d2cd9147316835429
sryung <sryung1225@gmail.com>
2023-04-14 15:47:10 +0900

    [migration] styles 폴더 스타일드 컴포넌트 파일들 변환 - @typescript-eslint/parser 설치 - .eslint.cjs 파일 내 parser 옵션 설정 : line18 - js->tsx 파일 변환 - FormButton, CustomButton 내 props 에러 대처를 위한 interface 추가

--- potato-market/.eslintrc.cjs
+++ potato-market/.eslintrc.cjs
@@ -1,99 +1,96 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:react/jsx-runtime',
-        'plugin:import/recommended',
-        'plugin:import/typescript',
-        'plugin:react-hooks/recommended',
-        'plugin:jsx-a11y/recommended',
-        'prettier',
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "import", "react-hooks", "jsx-a11y"],
+  rules: {
+    "import/no-unresolved": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": [
+      "warn",
+      {
+        additionalHooks: "useRecoilCallback",
+      },
     ],
-    "overrides": [
+    "no-console": "warn",
+    "react/prop-types": "off",
+    "react/button-has-type": "warn",
+    "react/self-closing-comp": [
+      "warn",
+      {
+        component: true,
+        html: false,
+      },
     ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        'import',
-        'react-hooks',
-        'jsx-a11y',
+    "react/jsx-sort-props": [
+      "warn",
+      {
+        shorthandFirst: true,
+        callbacksLast: true,
+        noSortAlphabetically: false,
+        reservedFirst: true,
+        multiline: "last",
+      },
     ],
-    "rules": {
-      "import/no-unresolved": "off",
-        'react-hooks/rules-of-hooks': 'error',
-		    'react-hooks/exhaustive-deps': ['warn',
-        {
-          "additionalHooks": "useRecoilCallback"
-        }],
-        'no-console': 'warn',
-        'react/prop-types': 'off',
-        'react/button-has-type': 'warn',
-        'react/self-closing-comp': [
-            'warn',
-            {
-                component: true,
-                html: false
-            }
+    "import/order": [
+      "warn",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          ["sibling", "parent", "index"],
+          "type",
+          "unknown",
         ],
-        'react/jsx-sort-props': [
-          'warn',
+        pathGroups: [
           {
-            shorthandFirst: true,
-            callbacksLast: true,
-            noSortAlphabetically: false,
-            reservedFirst: true,
-                multiline: 'last',
+            pattern: "{react*,react*/**}",
+            group: "external",
+            position: "before",
+          },
+          {
+            pattern: "{./**/*.module.css,./**/*.css}",
+            group: "unknown",
           },
         ],
-        'import/order': [
-            'warn',
-            {
-              groups: [
-                'builtin',
-                'external',
-                'internal',
-                ['sibling', 'parent', 'index'],
-                'type',
-                'unknown',
-              ],
-              pathGroups: [
-                {
-                  pattern: '{react*,react*/**}',
-                  group: 'external',
-                  position: 'before',
-                },
-                {
-                  pattern: '{./**/*.module.css,./**/*.css}',
-                  group: 'unknown',
-                },
-              ],
-              pathGroupsExcludedImportTypes: ['react', 'unknown'],
-              'newlines-between': 'always-and-inside-groups',
-              alphabetize: {
-                order: 'asc',
-                caseInsensitive: true,
-              },
-            },
-          ],
-          'jsx-a11y/anchor-has-content': [
-            'warn',
-            {
-              components: ['Link'],
-            },
-          ],
-          'jsx-a11y/anchor-is-valid': [
-            'warn',
-            {
-              components: ['Link'],
-            },
-          ],
-    },
-}
+        pathGroupsExcludedImportTypes: ["react", "unknown"],
+        "newlines-between": "always-and-inside-groups",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+    "jsx-a11y/anchor-has-content": [
+      "warn",
+      {
+        components: ["Link"],
+      },
+    ],
+    "jsx-a11y/anchor-is-valid": [
+      "warn",
+      {
+        components: ["Link"],
+      },
+    ],
+  },
+};
