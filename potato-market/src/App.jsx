32a2e1a7d27f695a78a4a2565f0432818dd7cc66
mkk <mk1210k@gmail.com>
2023-04-17 16:00:42 +0900

    [refact] 코드 스플리팅으로 렌더링 성능 개선(React.lazy 사용)

--- potato-market/src/App.jsx
+++ potato-market/src/App.jsx
@@ -1,22 +1,23 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, lazy, Suspense } from "react";
 
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 
-import { useRecoilState } from "recoil";
+import { useRecoilState, useSetRecoilState  } from "recoil";
 
-import Comment from './components/comment';
-import Footer from './components/Footer';
-import Header from './components/Header';
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
-import firebase from './firebase';
-import GlobalStyle from './styles/Global';
+import firebase from "./firebase";
+import GlobalStyle from "./styles/Global";
 
 import { userId, userInformation } from "@/stores/userAuth.js"
 
+const Comment = lazy(() => import("./components/comment"));
+
 function App() {
   const [userUid, setUserUid] = useRecoilState(userId);
-  const [, setUserInfo] = useRecoilState(userInformation);
-  const [lender, setLender] = useState(0);
+  const setUserInfo = useSetRecoilState (userInformation);
+  const [, setLender] = useState(0);
 
   // 로그인 상태 체크 (로그인/로그아웃/새로고침 시 실행)
   useEffect(() => {
@@ -25,7 +26,7 @@ function App() {
         const db = firebase.firestore();
         const userInfoRef = db.collection("users").doc(user.uid);
         userInfoRef.get().then((doc) => {
-          console.log(`로그인상태\nUID : ${user.uid} \n닉네임 : ${doc.data().nickname}`);
+          // console.log(`로그인상태\nUID : ${user.uid} \n닉네임 : ${doc.data().nickname}`);
           setUserUid(user.uid);
           setUserInfo({
             location: doc.data().location,
@@ -38,7 +39,7 @@ function App() {
           setLender(1);
         })
       } else {
-        console.log('로그아웃상태');
+        // console.log('로그아웃상태');
         setLender(1)
       }
     });
@@ -49,8 +50,10 @@ function App() {
       <GlobalStyle />
       <div className="App">
         <Header />
-        <Comment/>
-        {lender?<Outlet />:null}
+        <Suspense>
+          <Comment />
+        </Suspense>
+        <Outlet />
         <Footer />
       </div>
     </>
