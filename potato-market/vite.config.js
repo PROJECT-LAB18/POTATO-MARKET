102f84ad44edde86991187905bc880a0136364f1
Yujin <jyj15938@gmail.com>
2023-04-19 22:55:00 +0900

    [refact] 배포 이미지 플러그인 & makeWebp

--- potato-market/vite.config.js
+++ potato-market/vite.config.js
@@ -1,11 +1,31 @@
 import * as path from "node:path";
 
+import viteImagemin from "@vheemstra/vite-plugin-imagemin";
 import react from "@vitejs/plugin-react-swc";
+import imageminGifSicle from "imagemin-gifsicle";
+import imageminMozjpeg from "imagemin-mozjpeg";
+import imageminPngQuant from "imagemin-pngquant";
+import imageminSvgo from "imagemin-svgo";
+import imageminWebp from "imagemin-webp";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
-    react(),    
+    react(),
+    viteImagemin({
+      plugins: {
+        jpg: imageminMozjpeg(),
+        png: imageminPngQuant(),
+        gif: imageminGifSicle(),
+        svg: imageminSvgo(),
+      },
+      makeWebp: {
+        plugins: {
+          jpg: imageminWebp(),
+          png: imageminWebp(),
+        },
+      },
+    }),
   ],
   resolve: {
     alias: {
