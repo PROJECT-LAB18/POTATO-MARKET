52d04dca4046f2c7ef570bc43a97b6733da953e2
doctork4 <doctor.k1994@gmail.com>
2023-04-20 22:50:28 +0900

    [refact] api key값 dotenv 파일로 보호

--- potato-market/src/firebase.js
+++ potato-market/src/firebase.js
@@ -5,31 +5,22 @@ import "firebase/compat/firestore";
 import "firebase/compat/database"
 import "firebase/compat/storage";
 
-// const {
-//   VITE_API_KEY,
-//   VITE_AUTH_DOMAIN,
-//   VITE_PROJECT_ID,
-//   VITE_MESSAGE_SENDER_ID,
-//   VITE_STORAGE_BUCKET,
-//   VITE_APP_ID,
-// } = import.meta.env;
-
-// const firebaseConfig = {
-//   apiKey: VITE_API_KEY,
-//   authDomain: VITE_AUTH_DOMAIN,
-//   projectId: VITE_PROJECT_ID,
-//   storageBucket: VITE_STORAGE_BUCKET,
-//   messagingSenderId: VITE_MESSAGE_SENDER_ID,
-//   appId: VITE_APP_ID,
-// };
+const {
+  VITE_API_KEY,
+  VITE_AUTH_DOMAIN,
+  VITE_PROJECT_ID,
+  VITE_MESSAGE_SENDER_ID,
+  VITE_STORAGE_BUCKET,
+  VITE_APP_ID,
+} = import.meta.env;
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAOumwOGF_a3j3v5RqF_OzMEzNrlASGRyE",
-  authDomain: "potato-market-lab18.firebaseapp.com",
-  projectId: "potato-market-lab18",
-  storageBucket: "potato-market-lab18.appspot.com",
-  messagingSenderId: "862520392764",
-  appId: "1:862520392764:web:fa40b5561ca057dcfa5739",
+  apiKey: VITE_API_KEY,
+  authDomain: VITE_AUTH_DOMAIN,
+  projectId: VITE_PROJECT_ID,
+  storageBucket: VITE_STORAGE_BUCKET,
+  messagingSenderId: VITE_MESSAGE_SENDER_ID,
+  appId: VITE_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
